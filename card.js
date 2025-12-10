9f5d621bdef918e2cf0d7d63ff29565da3360d3f
H4kErMaN <132083093+H4kErMaN@users.noreply.github.com>
2025-12-10 15:42:34 +0200

    Delete card.js

--- card.js
+++
@@ -1,7 +0,0 @@
-export function createCardElement(emoji) {
-    const cardElement = document.createElement('div');
-    cardElement.classList.add('card');
-    cardElement.dataset.emoji = emoji;
-    cardElement.textContent = "";
-    return cardElement;
-}
